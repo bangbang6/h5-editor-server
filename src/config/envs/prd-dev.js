61e44bc0a4a0bb2770715bbfefc05491d4364eef
bangbang6 <577174667@qq.com>
2025-05-18 23:49:30 +0800

    feat: 自动测试

--- src/config/envs/prd-dev.js
+++ src/config/envs/prd-dev.js
@@ -1,14 +1,14 @@
 const devConf = require("./dev");
 Object.assign(devConf.redisConf, {
   port: "6378",
-  host: "docker-host", //host改为docker容器
+  host: "host.docker.internal", //host改为docker容器
 });
 Object.assign(devConf.mysqlConf, {
-  host: "docker-host", //host改为docker容器
+  host: "host.docker.internal", //host改为docker容器
   port: "3305",
 });
 Object.assign(devConf.mongodbConf, {
-  host: "docker-host", //host改为docker容器
+  host: "host.docker.internal", //host改为docker容器
   port: "27016",
 });
 module.exports = devConf;
